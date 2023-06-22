ba2bf595b757c6dd2fe8e87b3ca8bab3e36344ce
shashi-web <raoshashidhar.3@gmail.com>
2023-06-21 22:04:35 -0400

    updated code

--- main.tf
+++ main.tf
@@ -25,20 +25,6 @@ module "myapp_security_groups" {
 
 }
 
-
-module "myapp_server" {
-  source      = "./modules/instance"
-  vpc_id      = module.myapp_vpc.vpc_id
-  subnet_id   = module.myapp_subnet.subnet_id
-  image_name  = var.image_name
-  public_key  = var.public_key
-  security_group_id = module.myapp_security_groups.security_group_id
-  script_file = "jenkins-entry-script.sh"
-  server_name = "jenkins"
-  keyname = "jenkins"
-
-}
-
 module "nginx_server" {
   source      = "./modules/instance"
   vpc_id      = module.myapp_vpc.vpc_id
